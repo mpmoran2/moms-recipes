88c9876da2ccd7843d0c283e7c1d662a52f5c508
mpmoran <kamokumoran@gmail.com>
2020-11-21 00:12:11 -0800

    3/10 done

--- moms-recipe-client/src/appContainer.js
+++ moms-recipe-client/src/appContainer.js
@@ -1,20 +1,33 @@
 class AppContainer {
-    recipes = []
+    static recipes = []
     categories = []
     url = "http://localhost:3000"
     getFood = []
 
     bindEventListeners() {
-        const btn = document.getElementById('createGetFood');
-        
+        const btn = document.getElementById('createGetFood');    
+        btn.addEventListener('click', this.getRandomRecipies)            
+    };
+
+    getRandomRecipes() {
         debugger
+        let randomRecipes = [];
+        for (let i = 0; i < 2; i++) {
+            randomRecipes.push(AppContainer.recipes[Math.floor(Math.random()*AppContainer.recipes.length)]);
+        };
+        return randomRecipes;
     };
 
     getRecipes() {
         console.log('beep beep Imma sheep')
         fetch(this.url + '/recipes')
         .then(resp => resp.json())
-        .then(data => console.log(data))
+        .then(data => {
+            data.forEach(recipe => {
+                new Recipe(recipe.name)
+            })
+            
+        })
         //call render
         .catch(err => alert(err))
     };
--- moms-recipe-client/src/recipe.js
+++ moms-recipe-client/src/recipe.js
@@ -1,5 +1,6 @@
 class Recipe {
     constructor(name){
         this.name = name;
+        AppContainer.recipies.push(this);
     }
 }
